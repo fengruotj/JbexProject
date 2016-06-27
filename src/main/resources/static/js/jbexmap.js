36491687b0db75554ead32048936562760dab228
谭杰 <798750509@qq.com>
2016-06-28 03:35:35 +0800

    2016/6/28 3：35

--- src/main/resources/static/js/jbexmap.js
+++ src/main/resources/static/js/jbexmap.js
@@ -13,7 +13,7 @@ function getJbexinfoList(data,map,markers){
         var result=data.result;
         for(var i=0;i<result.length;i++){
             var item=result[i];
-            var content="<lable class='label label-primary'>"+"标题: "+item.title+"<br/>"+"细节:"+item.detail+"</lable>";
+            var content="<lable class='label label-primary'>"+"标题: "+item.title+"<br/>"+"细节: "+item.detail+"</lable>";
             //var content=item.title;
             var marker=addMakreOnMap(item.dotX,item.dotY,content,map);
             markers.push(marker);
